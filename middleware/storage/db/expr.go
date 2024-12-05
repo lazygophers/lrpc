520060991b7c30e6c3942cb53fd95ec37ce8f5e0
nico <nico@starpay.com>
2024-12-05 17:41:06 +0800

    if工具包

--- middleware/storage/db/expr.go
+++ middleware/storage/db/expr.go
@@ -1,8 +1,25 @@
 package db
 
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+var DefaultDriver = "mysql"
+
 func Expr(expression string, args ...interface{}) map[string]any {
 	return map[string]any{
 		"expr": expression,
 		"args": args,
+	}
+}
+
+func If(expr string, ok, nok interface{}) clause.Expr {
+	switch DefaultDriver {
+	case "sqlite", "sqlite3":
+		return gorm.Expr(fmt.Sprintf("IIF(%s, ?, ?)", expr), ok, nok)
+	default:
+		return gorm.Expr(fmt.Sprintf("IF(%s, ?, ?)", expr), ok, nok)
 	}
 }
